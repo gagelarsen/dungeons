af3a653b4eecd3472b33d5228ba0cc298fc3d40d
Gage Larsen <gagelarsen53@gmail.com>
2021-07-26 13:47:54 -0600

    add type hints

--- tests/test_dungeon.py
+++ tests/test_dungeon.py
@@ -3,7 +3,7 @@ from dungeons.dungeon import Dungeon
 from dungeons.geometry import directions
 
 
-def test_dungeon():
+def test_dungeon() -> None:
     """A test to check the base initialization of a dungeon."""
     dungeon = Dungeon(width=10, height=20)
     assert dungeon.width == 10
@@ -11,19 +11,19 @@ def test_dungeon():
     assert dungeon.randomness == 0.5
 
 
-def test_dungeon_randomness_max():
+def test_dungeon_randomness_max() -> None:
     """A test to check the max of a dungeons randomness."""
     dungeon = Dungeon(width=10, height=20, randomness=10)
     assert dungeon.randomness == 1
 
 
-def test_dungeon_randomness_min():
+def test_dungeon_randomness_min() -> None:
     """A test to check the min of a dungeons randomness."""
     dungeon = Dungeon(width=10, height=20, randomness=-4)
     assert dungeon.randomness == 0
 
 
-def test_print_dungeon(capsys):
+def test_print_dungeon(capsys: object) -> None:
     """A test to check the printing of the dungeon."""
     dungeon = Dungeon(width=20, height=10, random_seed=2)
     dungeon.print_dungeon()
@@ -44,14 +44,14 @@ def test_print_dungeon(capsys):
     assert captured.out == expected
 
 
-def test_can_open_room_cell_is_not_a_wall():
+def test_can_open_room_cell_is_not_a_wall() -> None:
     """Test you can't open a door outside of the dungeon."""
     dungeon = Dungeon(width=20, height=10, random_seed=2)
     can_carve = dungeon._can_carve_entry(2, 2, None)
     assert can_carve is False
 
 
-def test_can_open_room_cell_is_not_next_to_walls():
+def test_can_open_room_cell_is_not_next_to_walls() -> None:
     """Test you can't open a door outside of the dungeon."""
     dungeon = Dungeon(width=20, height=10, random_seed=2)
     dungeon._set_dungeon_cell(7, 3, dungeon.DUNGEON_ROOM)
